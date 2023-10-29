66fc11966803f092f72f7a03f3fca2f3ed852501
valselviz <valeriaselviz@gmail.com>
2023-10-29 20:30:18 -0300

    STORY1: Adding player status

--- src/components/DuelBoard/DuelBoard.tsx
+++ src/components/DuelBoard/DuelBoard.tsx
@@ -4,6 +4,7 @@ import { useEffect, useMemo, useState } from "react";
 import { ReactDuel } from "../../react-duel/react-duel";
 import CardsRow from "../CardsRow/CardsRow";
 import Hand from "../Hand/Hand";
+import PlayerStatus from "../PlayerStatus/PlayerStatus";
 
 import styles from "./DuelBoard.module.css";
 import { Player } from "../../game-logic/player";
@@ -40,22 +41,42 @@ export default function DuelBoard({ players }: DuelBoardProps) {
   return (
     <div>
       <div className={styles["field"]}>
-        <Hand
-          cards={cardsState[1][Zone.Hand]}
-          executeOneActionWithDelay={executeOneActionWithDelay}
-        />
-        <CardsRow
-          cards={cardsState[1][Zone.Field]}
-          executeOneActionWithDelay={executeOneActionWithDelay}
-        />
-        <CardsRow
-          cards={cardsState[0][Zone.Field]}
-          executeOneActionWithDelay={executeOneActionWithDelay}
-        />
-        <Hand
-          cards={cardsState[0][Zone.Hand]}
-          executeOneActionWithDelay={executeOneActionWithDelay}
-        />
+        <div
+          className={styles["playerSide"] + " " + styles["playerSide-top"]}
+        >
+          <Hand
+            cards={cardsState[1][Zone.Hand]}
+            executeOneActionWithDelay={executeOneActionWithDelay}
+          />
+          <CardsRow
+            cards={cardsState[1][Zone.Field]}
+            executeOneActionWithDelay={executeOneActionWithDelay}
+          />
+        </div>
+        <div className={styles["playersStatusDiv"]}>
+          <PlayerStatus
+            playerName={players[1].name}
+            playerDeckCards={cardsState[1][Zone.Deck].length}
+            playerId={1}
+          />
+          <PlayerStatus
+            playerName={players[0].name}
+            playerDeckCards={cardsState[0][Zone.Deck].length}
+            playerId={0}
+          />
+        </div>
+        <div
+          className={styles["playerSide"] + " " + styles["playerSide-bottom"]}
+        >
+          <CardsRow
+            cards={cardsState[0][Zone.Field]}
+            executeOneActionWithDelay={executeOneActionWithDelay}
+          />
+          <Hand
+            cards={cardsState[0][Zone.Hand]}
+            executeOneActionWithDelay={executeOneActionWithDelay}
+          />
+        </div>
       </div>
     </div>
   );
