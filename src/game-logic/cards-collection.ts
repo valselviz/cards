0429bfcb50c9cadc59b35e81a29aa9edfa305fdc
alfonsoesteves93 <alfonsoesteves93@gmail.com>
2023-12-16 14:59:56 -0300

    Add alert error messaged

--- src/game-logic/cards-collection.ts
+++ src/game-logic/cards-collection.ts
@@ -18,11 +18,25 @@ function simpleInvokation(card: Card) {
 }
 
 function oneSacrificeInvokation(card: Card) {
-  if (card.duel.cards[card.playerId][Zone.Field].length > 0) {
-    card.duel.startSelection(card.playerId, Zone.Field);
-    card.duel.destroy(() => card.duel.selectedTarget);
-    card.duel.invoke(() => card);
+  if (card.duel.cards[card.playerId][Zone.Field].length == 0) {
+    alert("You need one card in the field to offer as sacrifice.");
+    return;
   }
+  card.duel.startSelection(card.playerId, Zone.Field);
+  card.duel.destroy(() => card.duel.selectedTarget);
+  card.duel.invoke(() => card);
+}
+
+function twoSacrificesInvokation(card: Card) {
+  if (card.duel.cards[card.playerId][Zone.Field].length < 2) {
+    alert("You need two cards in the field to offer as sacrifices.");
+    return;
+  }
+  card.duel.startSelection(card.playerId, Zone.Field);
+  card.duel.destroy(() => card.duel.selectedTarget);
+  card.duel.startSelection(card.playerId, Zone.Field);
+  card.duel.destroy(() => card.duel.selectedTarget);
+  card.duel.invoke(() => card);
 }
 
 function simpleAttack(card: Card) {
@@ -41,6 +55,8 @@ function simpleAttack(card: Card) {
       () => card,
       () => card.duel.selectedTarget
     );
+  } else {
+    alert("Your opponent cards have too much defense to be attacked.");
   }
 }
 
@@ -60,16 +76,16 @@ export const cardModels: any = {
     25,
     10,
     Color.Red,
-    simpleInvokation,
+    oneSacrificeInvokation,
     simpleAttack
   ),
   Golem: new CardModel(
     "Golem",
     golem,
-    10,
+    12,
     29,
     Color.Red,
-    simpleInvokation,
+    oneSacrificeInvokation,
     simpleAttack
   ),
   HammerDwarf: new CardModel(
@@ -96,7 +112,7 @@ export const cardModels: any = {
     35,
     25,
     Color.Blue,
-    oneSacrificeInvokation,
+    twoSacrificesInvokation,
     simpleAttack
   ),
   Vortex: new CardModel(
@@ -106,26 +122,32 @@ export const cardModels: any = {
     0,
     Color.Yellow,
     (card: Card) => {
-      const destroyCriteria = (availableCard: Card) =>
-        availableCard.model.defense <= 20;
-      if (
-        card.duel.cards[card.playerId][Zone.Hand].length > 1 &&
-        card.duel.cards[1 - card.playerId][Zone.Field].some(destroyCriteria)
-      ) {
-        card.duel.startSelection(
-          card.playerId,
-          Zone.Hand,
-          (availableCard) => availableCard !== card
+      if (card.duel.cards[card.playerId][Zone.Hand].length < 2) {
+        alert(
+          "You need an additional card in your hand to offer as sacrifice."
         );
-        card.duel.discard(() => card.duel.selectedTarget);
-        card.duel.startSelection(
-          1 - card.playerId,
-          Zone.Field,
-          destroyCriteria
-        );
-        card.duel.destroy(() => card.duel.selectedTarget);
-        card.duel.discard(() => card);
+        return;
       }
+      const destroyCriteriaDefense = 20;
+      const destroyCriteria = (availableCard: Card) =>
+        availableCard.model.defense <= destroyCriteriaDefense;
+      if (
+        !card.duel.cards[1 - card.playerId][Zone.Field].some(destroyCriteria)
+      ) {
+        alert(
+          `You opponent does not have any card with ${destroyCriteriaDefense} defense or less.`
+        );
+        return;
+      }
+      card.duel.startSelection(
+        card.playerId,
+        Zone.Hand,
+        (availableCard) => availableCard !== card
+      );
+      card.duel.discard(() => card.duel.selectedTarget);
+      card.duel.startSelection(1 - card.playerId, Zone.Field, destroyCriteria);
+      card.duel.destroy(() => card.duel.selectedTarget);
+      card.duel.discard(() => card);
     },
     () => null
   ),
@@ -155,6 +177,8 @@ export const cardModels: any = {
           () => card.duel.selectedTarget
         );
         card.duel.draw(card.playerId);
+      } else {
+        alert("Your opponent cards have too much defense to be attacked.");
       }
     }
   ),
