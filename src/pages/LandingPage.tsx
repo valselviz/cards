43b33c6cccb85d8a44508f0865cd62a7c24ca0ee
valselviz <valeriaselviz@gmail.com>
2024-01-04 16:36:10 -0300

    Update readme

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,10 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function LandingPage() {
   return (
-    <>
-      <title>Collectable Cards Game</title>
+    <div>
+      <h1>Collectable Cards Game</h1>
+      <p>by Valeria Selviz</p>
       <Link to="/home">Continue</Link>
       <Link to="/home">Start New Game</Link>
-    </>
+    </div>
   );
 }
