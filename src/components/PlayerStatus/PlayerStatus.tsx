564d8b0ac7630ec525ad3b9164b2b104b38ef069
valselviz <134339028+valselviz@users.noreply.github.com>
2024-01-17 20:23:20 -0300

    STORY33: damaged player animation (#27)

--- src/components/PlayerStatus/PlayerStatus.tsx
+++ src/components/PlayerStatus/PlayerStatus.tsx
@@ -1,23 +1,36 @@
+import { useState } from "react";
 import styles from "./PlayerStatus.module.css";
 import damagedPlayerStatusBox from "assets/damagedStatusBox.png";
+import { ReactDuelUI } from "ReactDuelUI/ReactDuelUI";
 
 interface PlayerStatusProps {
   playerName: string;
   playerDeckCards: number;
   playerId: number;
-  playerTurn: number;
+  duelUI: ReactDuelUI;
 }
 
 export default function PlayerStatus({
   playerName,
   playerDeckCards,
+  playerId,
+  duelUI,
 }: PlayerStatusProps) {
+  const [damaged, setDamaged] = useState(false);
+
+  duelUI.setDamaged[playerId] = setDamaged;
+
+  const damagedAnimation = damaged
+    ? styles.damagedPlayerStatus
+    : styles.invisible;
+
   return (
     <div>
       <img
-        className={styles.damagedPlayerStatus}
+        className={damagedAnimation}
         src={damagedPlayerStatusBox}
         alt=""
+        onAnimationEnd={() => setDamaged(false)}
       ></img>
       <div className={styles.playerStatus}>
         <p className={styles.statusText}>{playerName}</p>
