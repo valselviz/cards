150f3fe47ba4b4f54a7ecdc1255995ca69985902
valselviz <134339028+valselviz@users.noreply.github.com>
2024-03-03 18:10:14 -0300

    Story36 (#33)
    * STORY36 finish landing page

--- src/pages/landing-page/LandingPage.tsx
+++ src/pages/landing-page/LandingPage.tsx
@@ -10,129 +10,149 @@ export default function LandingPage() {
   const [email, setEmail] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
+  const [showLoginForm, setShowLoginForm] = useState(true);
+
   const context = useContext(MacroGameContext);
+
+  const loginButtonStyles = showLoginForm ? styles.activeButton : "";
+  const signupButtonStyles = !showLoginForm ? styles.activeButton : "";
 
   return (
     <div className={styles.landingPageDiv}>
-      <h1>Collectable Cards Game</h1>
-      <h3>By Valeria Selviz</h3>
-      <button>Continue</button>
-      <button>New Game</button>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          try {
-            const loginResponse = await login(username, password);
-            context.username = username;
-            if (loginResponse === "") {
-              // If the table record does not have a macrogame, create a new one
-              context.macrogame = new MacroGame();
-              await update(username, context.macrogame);
-            } else {
-              context.macrogame = loginResponse;
-            }
-            window.location.href = "/#/deck";
-          } catch (error) {
-            if (error instanceof Error) {
-              if (error.message === "404") {
-                alert(`Player ${username} does not exist`);
+      <h1>Valeria CCG</h1>
+      <div className={styles.formContainerDiv}>
+        <div className={styles.buttonDiv}>
+          <button
+            className={loginButtonStyles}
+            onClick={() => setShowLoginForm(true)}
+          >
+            Continue
+          </button>
+          <button
+            className={signupButtonStyles}
+            onClick={() => setShowLoginForm(false)}
+          >
+            New Game
+          </button>
+        </div>
+        {showLoginForm && (
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              try {
+                const loginResponse = await login(username, password);
+                context.username = username;
+                if (loginResponse === "") {
+                  // If the table record does not have a macrogame, create a new one
+                  context.macrogame = new MacroGame();
+                  await update(username, context.macrogame);
+                } else {
+                  context.macrogame = loginResponse;
+                }
+                window.location.href = "/#/deck";
+              } catch (error) {
+                if (error instanceof Error) {
+                  if (error.message === "404") {
+                    alert(`Player ${username} does not exist`);
+                    return;
+                  }
+                  if (error.message === "401") {
+                    alert(`Wrong Password`);
+                    return;
+                  }
+                  if (error.message !== "200") {
+                    alert(`Unexpected error`);
+                    return;
+                  }
+                }
+              }
+            }}
+          >
+            <label>
+              Username{" "}
+              <input
+                type="text"
+                autoComplete="one-time-code"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+            <label>
+              Password{" "}
+              <input
+                type="password"
+                autoComplete="one-time-code"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <input type="submit" value="Login" />
+          </form>
+        )}
+        {!showLoginForm && (
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              if (password !== confirmPassword) {
+                alert("Password confirmation does not match");
                 return;
               }
-              if (error.message === "401") {
-                alert(`Wrong Password`);
-                return;
-              }
-              if (error.message !== "200") {
-                alert(`Unexpected error`);
-                return;
-              }
-            }
-          }
-        }}
-      >
-        <label>
-          Username:{" "}
-          <input
-            type="text"
-            name="name"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <label>
-          Password:{" "}
-          <input
-            type="password"
-            name="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <input type="submit" value="Login" />
-        <button>Back</button>
-      </form>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          if (password !== confirmPassword) {
-            alert("Password confirmation does not match");
-            return;
-          }
-          try {
-            await signup(username, password, email);
-            context.username = username;
-            context.macrogame = new MacroGame();
-            await update(username, context.macrogame);
+              try {
+                await signup(username, password, email);
+                context.username = username;
+                context.macrogame = new MacroGame();
+                await update(username, context.macrogame);
 
-            window.location.href = "/#/deck";
-          } catch (error) {
-            if (error instanceof Error) {
-              if (error.message === "409") {
-                alert(`Username already taken`);
-                return;
+                window.location.href = "/#/deck";
+              } catch (error) {
+                if (error instanceof Error) {
+                  if (error.message === "409") {
+                    alert(`Username already taken`);
+                    return;
+                  }
+                  if (error.message !== "200") {
+                    alert(`Unexpected error calling create player endpoint`);
+                    return;
+                  }
+                }
               }
-              if (error.message !== "200") {
-                alert(`Unexpected error calling create player endpoint`);
-                return;
-              }
-            }
-          }
-        }}
-      >
-        <label>
-          Username:{" "}
-          <input
-            type="text"
-            name="name"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <label>
-          Email:{" "}
-          <input
-            type="text"
-            name="name"
-            placeholder="optional"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label>
-          Password:{" "}
-          <input
-            type="password"
-            name="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <label>
-          Confirm Password:{" "}
-          <input
-            type="password"
-            name="password"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </label>
-        <input type="submit" value="Sign Up" />
-        <button>Back</button>
-      </form>
+            }}
+          >
+            <label>
+              Username{" "}
+              <input
+                type="text"
+                autoComplete="one-time-code"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+            <label>
+              Email{" "}
+              <input
+                type="text"
+                autoComplete="one-time-code"
+                placeholder="optional"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <label>
+              Password{" "}
+              <input
+                type="password"
+                autoComplete="one-time-code"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <label>
+              Confirm Password{" "}
+              <input
+                type="password"
+                autoComplete="one-time-code"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </label>
+            <input type="submit" value="Sign Up" />
+          </form>
+        )}
+      </div>
     </div>
   );
 }
