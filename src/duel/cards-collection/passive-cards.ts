e49cc81718b1d83520ce90e1f7991c1e18c8a48d
valselviz <134339028+valselviz@users.noreply.github.com>
2024-03-30 18:49:43 -0300

    STORY47: new card: Fiend (#40)

--- src/duel/cards-collection/passive-cards.ts
+++ src/duel/cards-collection/passive-cards.ts
@@ -9,7 +9,8 @@ import {
   simpleInvokationInfo,
 } from "./cards-collection";
 import ballista from "../../assets/cards/ballista.jpg";
-import whiteKnight from "../../assets/cards/whiteKnight.jpg"
+import whiteKnight from "../../assets/cards/whiteKnight.jpg";
+import fiend from "../../assets/cards/fiend.jpg";
 import { Color } from "duel/color";
 import { Card } from "duel/Card";
 import { DuelEvent } from "duel/DuelEvent";
@@ -43,5 +44,20 @@ export function loadPassiveCards() {
         if (!event.target || event.target.model.attack < 15) return;
         card.duel.queueDrawAction(event.target.playerId);
       }, "When a player invokes a card with 15 attack or more, he draws a card.")
+  );
+
+  addCardModel(
+    new CardModel(66, "Fiend", fiend, 5, 0, Color.Red, 1.7, [
+      labelNoSacrifice,
+      labelPassive,
+    ])
+      .withHandEffect(simpleInvokation, simpleInvokationInfo)
+      .withFieldEffect(simpleAttack, simpleAttackInfo)
+      .withPassiveEffect((card: Card, event: DuelEvent) => {
+        if (event.eventType !== EventType.Attack) return;
+        if (!event.target || !event.source) return;
+        if (event.target !== card) return;
+        card.duel.queueDestroyAction(() => event.source);
+      }, "The card that attacks Fiend is destroyed.")
   );
 }
